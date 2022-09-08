7b17049ff98b8d3efca675ef8eaa2e60420012cc
gosuljo <lurutia@naver.com>
2022-09-08 22:06:40 +0900

    JobInstanceConfiguration 추가 JobParameter 확인을 위한 JobRunner 추가 Job이 자동실행되지 않도록 설정 변경

---
+++ spring-batch-start/demo/src/main/java/com/example/demo/config/JobInstanceConfiguration.java
@@ -0,0 +1,43 @@
+package com.example.demo.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@RequiredArgsConstructor
+public class JobInstanceConfiguration {
+    private final JobBuilderFactory jobBuilderFactory;
+    private final StepBuilderFactory stepBuilderFactory;
+
+    @Bean
+    public Job jobInstanceJob() {
+        return jobBuilderFactory.get("job")
+                .start(jobInstanceStep1())
+                .next(jobInstanceStep2())
+                .build();
+    }
+
+    @Bean
+    public Step jobInstanceStep1() {
+        return stepBuilderFactory.get("step1")
+                .tasklet((stepContribution, chunkContext) -> {
+                    return RepeatStatus.FINISHED;
+                })
+                .build();
+    }
+
+    @Bean
+    public Step jobInstanceStep2() {
+        return stepBuilderFactory.get("step1")
+                .tasklet((stepContribution, chunkContext) -> {
+                    return RepeatStatus.FINISHED;
+                })
+                .build();
+    }
+}
---
+++ spring-batch-start/demo/src/main/java/com/example/demo/config/JobRunner.java
@@ -0,0 +1,29 @@
+package com.example.demo.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersBuilder;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.Resource;
+
+@Component
+@RequiredArgsConstructor
+public class JobRunner implements ApplicationRunner {
+
+    private final JobLauncher jobLauncher;
+    @Resource(name = "jobInstanceJob")
+    private final Job job;
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        JobParameters jobParameters = new JobParametersBuilder()
+                .addString("name", "user1")
+                .toJobParameters();
+        jobLauncher.run(job, jobParameters);
+    }
+}
