8cfb7fd66c801110ff4d75e9d1b842c8ec5252f3
gosuljo <lurutia@naver.com>
2022-09-18 12:35:07 +0900

    SimpleJobconfiguration 추가

---
+++ spring-batch-start/demo/src/main/java/com/example/demo/simpleJobConfiguration/SimpleJobConfiguration.java
@@ -0,0 +1,81 @@
+package com.example.demo.simpleJobConfiguration;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.batch.core.*;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@RequiredArgsConstructor
+public class SimpleJobConfiguration {
+
+    private final JobBuilderFactory jobBuilderFactory;
+    private final StepBuilderFactory stepBuilderFactory;
+
+    @Bean
+    public Job job() {
+        return jobBuilderFactory.get("batchJob")
+                .start(step1())
+                .next(step2())
+                .next(step3())
+                .incrementer(new RunIdIncrementer())
+                .validator((jobParameters) -> {
+
+                })
+                .preventRestart()
+                .listener(new JobExecutionListener() {
+                    @Override
+                    public void beforeJob(JobExecution jobExecution) {
+
+                    }
+
+                    @Override
+                    public void afterJob(JobExecution jobExecution) {
+
+                    }
+                })
+                .build();
+    }
+
+    @Bean
+    public Step step1() {
+        return stepBuilderFactory.get("step1")
+                .tasklet(((stepContribution, chunkContext) -> {
+                    System.out.println("=========================");
+                    System.out.println(" >> step 1 was executed");
+                    System.out.println("=========================");
+                    return RepeatStatus.FINISHED;
+                }))
+                .build();
+    }
+
+    @Bean
+    public Step step2() {
+        return stepBuilderFactory.get("step2")
+                .tasklet(((stepContribution, chunkContext) -> {
+                    System.out.println("=========================");
+                    System.out.println(" >> step 2 was executed");
+                    System.out.println("=========================");
+                    return RepeatStatus.FINISHED;
+                }))
+                .build();
+    }
+
+    @Bean
+    public Step step3() {
+        return stepBuilderFactory.get("step3")
+                .tasklet(((stepContribution, chunkContext) -> {
+                    chunkContext.getStepContext().getStepExecution().setStatus(BatchStatus.FAILED);
+                    stepContribution.setExitStatus(ExitStatus.STOPPED);
+                    System.out.println("=========================");
+                    System.out.println(" >> step 3 was executed");
+                    System.out.println("=========================");
+                    return RepeatStatus.FINISHED;
+                }))
+                .build();
+    }
+}
