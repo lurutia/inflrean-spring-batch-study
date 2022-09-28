8339d45a7f528f1fdfe99be114493d3c121f34cd
gosuljo <lurutia@naver.com>
2022-09-28 23:50:19 +0900

    TransitionConfiguration 추가

--- spring-batch-start/demo/src/main/java/com/example/demo/config/TransitionConfiguration.java
+++ spring-batch-start/demo/src/main/java/com/example/demo/config/TransitionConfiguration.java
@@ -1,6 +1,7 @@
 package com.example.demo.config;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.batch.core.ExitStatus;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
@@ -11,37 +12,44 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @RequiredArgsConstructor
-public class FlowJobConfiguration {
+public class TransitionConfiguration {
 
-    private final JobBuilderFactory jobBuilderFactory;
     private final StepBuilderFactory stepBuilderFactory;
+    private final JobBuilderFactory jobBuilderFactory;
 
-//    @Bean
-    public Job batchJob() {
+        @Bean
+    public Job job() {
         return jobBuilderFactory.get("batchJob")
                 .start(step1())
-                .on("COMPLETED").to(step3())
+                    .on("FAILED")
+                    .to(step2())
+                    .on("FAILED")
+                    .stop()
                 .from(step1())
-                .on("FAILED")
-                .to(step2())
+                    .on("*")
+                    .to(step3())
+                    .next(step4())
+                .from(step2())
+                    .on("*")
+                    .to(step5())
                 .end()
                 .build();
     }
 
-//    @Bean
+    @Bean
     public Step step1() {
         return stepBuilderFactory.get("step1")
                 .tasklet(((stepContribution, chunkContext) -> {
-                    throw new RuntimeException("failed");
-//                    System.out.println("=========================");
-//                    System.out.println(" >> step 1 was executed");
-//                    System.out.println("=========================");
-//                    return RepeatStatus.FINISHED;
+                    System.out.println("=========================");
+                    System.out.println(" >> step 1 was executed");
+                    System.out.println("=========================");
+//                    stepContribution.setExitStatus(ExitStatus.FAILED);
+                    return RepeatStatus.FINISHED;
                 }))
                 .build();
     }
 
-//    @Bean
+    @Bean
     public Step step2() {
         return stepBuilderFactory.get("step2")
                 .tasklet(((stepContribution, chunkContext) -> {
@@ -53,7 +61,7 @@ public class FlowJobConfiguration {
                 .build();
     }
 
-//    @Bean
+    @Bean
     public Step step3() {
         return stepBuilderFactory.get("step3")
                 .tasklet(((stepContribution, chunkContext) -> {
@@ -64,4 +72,29 @@ public class FlowJobConfiguration {
                 }))
                 .build();
     }
+
+    @Bean
+    public Step step4() {
+        return stepBuilderFactory.get("step4")
+                .tasklet(((stepContribution, chunkContext) -> {
+                    System.out.println("=========================");
+                    System.out.println(" >> step 4 was executed");
+                    System.out.println("=========================");
+                    return RepeatStatus.FINISHED;
+                }))
+                .build();
+    }
+
+    @Bean
+    public Step step5() {
+        return stepBuilderFactory.get("step5")
+                .tasklet(((stepContribution, chunkContext) -> {
+                    System.out.println("=========================");
+                    System.out.println(" >> step 5 was executed");
+                    System.out.println("=========================");
+                    return RepeatStatus.FINISHED;
+                }))
+                .build();
+    }
 }
+
